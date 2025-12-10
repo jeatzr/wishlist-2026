71fe8f141475431373e7ac60dceb6015932ee6a6
jeatzr <profesor.jeatienzar@iesazarquiel.es>
2025-12-10 09:18:09 +0100

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+let wishes = [
+  {
+    id: 0,
+    text: "Been millionaire",
+    completed: false,
+  },
+  {
+    id: 1,
+    text: "Pass this beautiful subject",
+    completed: false,
+  },
+  {
+    id: 2,
+    text: "Eat a burger",
+    completed: true,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <h1>My Wishlist</h1>
+
+      <fieldset className="wish-input">
+        <legend className="wish-input__label">New wish:</legend>
+        <input type="text" placeholder="My new wish" />
+      </fieldset>
+      <ul className="wish-list">
+        {wishes.map((wish) => (
+          <li
+            key={wish.id}
+            className={`wish-list__item 
+              ${wish.completed && "wish-list__item--done"}
+              `}
+          >
+            <input type="checkbox" checked={wish.completed} id={wish.id} />
+            <label htmlFor={wish.id}>{wish.text}</label>
+          </li>
+        ))}
+      </ul>
+      <button type="button">Archive all wishes</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
